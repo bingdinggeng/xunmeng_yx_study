9beb612730e35371d5129468f96433bf9fd11c12
bingdinggeng <128678313+bingdinggeng@users.noreply.github.com>
2023-05-13 17:56:12 +0800

    添加实体类对应的mapper和xml文件

---
+++ src/main/java/com/xunmeng/mapper/XmAdminMapper.java
@@ -0,0 +1,17 @@
+package com.xunmeng.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xunmeng.domain.XmAdmin;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * ClassName: XmAdminMapper
+ * Package: com.xunmeng.mapper
+ * Description:
+ *
+ * @Author LTM
+ * @Create 2023/5/13 16:59
+ * @Version 1.0
+ */
+public interface XmAdminMapper extends BaseMapper<XmAdmin> {
+}
