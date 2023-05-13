89bd14b2f709a637e43cf6ec21f51c32415ba0e7
bingdinggeng <128678313+bingdinggeng@users.noreply.github.com>
2023-05-13 14:35:40 +0800

    在pom文件中添加<dependency>              <groupId>io.springfox</groupId>              <artifactId>springfox-boot-starter</artifactId>              <version>3.0.0</version>          </dependency>          依赖后成功启动/修改了启动端口

--- src/main/java/com/xunmeng/YxStudyApplication.java
+++ src/main/java/com/xunmeng/YxStudyApplication.java
@@ -1,5 +1,6 @@
 package com.xunmeng;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,9 +16,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableAsync
 @SpringBootApplication
-@MapperScan("com.xunmeng.youxuan.mapper")
-@EnableScheduling
-@ServletComponentScan
 public class YxStudyApplication {
 
     public static void main(String[] args) {
