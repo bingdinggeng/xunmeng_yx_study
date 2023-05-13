16e430c99751c28ddd8ea377c14ffee593357253
bingdinggeng <128678313+bingdinggeng@users.noreply.github.com>
2023-05-13 11:29:11 +0800

    初次上传，完成pom配置

---
+++ src/main/java/com/xunmeng/XunmengYxStudyApplication.java
@@ -0,0 +1,13 @@
+package com.xunmeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunmengYxStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XunmengYxStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xunmeng/XunmengYxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xunmeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XunmengYxStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
